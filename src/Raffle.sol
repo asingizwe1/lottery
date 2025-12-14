171a4bf38bcf7650c27120a6291e971584f28483
asingizwe1 <asingizwe1louis1@gmail.com>
2025-12-14 22:52:19 +0300

    _raffle_refinement

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -3,14 +3,6 @@ pragma solidity ^0.8.19;
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
-uint256 private immutable i_interval;
-bytes32 private immutable i_keyHash;
-address payable[] private s_players;
-uint256 private s_lastTimestamp;
-//
-address private s_recentWinner; //to keep track of the most recent winner
-address private _recentWinner; 
-uint32 private constant NUM_WORDS = 1; //number of random words we want to get
 /**
  *@title Raffle contract
  *@author Louis Asingizwe
@@ -19,6 +11,16 @@ uint32 private constant NUM_WORDS = 1; //number of random words we want to get
  */
 
 contract Raffle is VRFConsumerBaseV2Plus {
+//immutable variables must be assigned either at declaration or inside the constructor
+uint256 private immutable i_interval;
+bytes32 private immutable i_keyHash;
+address payable[] private s_players;
+uint256 private s_lastTimestamp;
+//
+address private s_recentWinner; //to keep track of the most recent winner
+
+uint32 private constant NUM_WORDS = 1; //number of random words we want to get
+
     /**ERRORS */
     error Raffle__UpkeepNotNeeded(uint256 balance, uint256 playersLength, uint256 raffleState); //custom error to handle upkeep not needed
 error Raffle_TransferFailed(); //custom error to handle transfer failure
@@ -64,7 +66,7 @@ event WinnerPicked(address indexed winner);
     constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator,uint256 gasLane,uint256 ,uint32 callbackGasLimit) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
-        i_callbackGasLimit=callbackGasLimit
+        i_callbackGasLimit=callbackGasLimit;
         //when we work with chain link VRF every node will get its own subscription id
 i_subscriptionId=subscriptionId;
         s_lastTimestamp = block.timestamp;
@@ -72,7 +74,7 @@ i_subscriptionId=subscriptionId;
     }
     //we can now request random words from the VRF coordinator
     //this function will be called when we want to pick a winner
-VRF2PlusClient.VRF2PlusRequest memory request = VRF2PlusClient.RandomWordsRequest({
+/*VRF2PlusClient.RandomWordsRequest memory request = VRF2PlusClient.RandomWordsRequest({
         keyHash: i_keyHash,
         subId: i_subscriptionId,
         requestConfirmations: REQUEST_CONFIRMATIONS,
@@ -86,7 +88,7 @@ VRF2PlusClient.VRF2PlusRequest memory request = VRF2PlusClient.RandomWordsReques
     }//once we send the request above the chain link nide will wait for some blocks ..it will then generate random number 
     //via fulfillRandomWords and thats how we shall get random number back 
 
-
+*/
 //chainlink automation to automatically pick winner after a certain time interval
 
 
@@ -146,8 +148,9 @@ function checkUpkeep(bytes calldata /*checkData*/) public view returns (bool upk
         extraArgs: VRF2PlusClient._argsToBytes(
             VRF2PlusClient.ExtraArgsV1({nativePayment: false})
         )
-    });
-    uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+    });// this is how we are calling chainlink VRF to get random number
+    
+   s_vrfCoordinator.requestRandomWords(request);
         //we call the pick winner function
 
     }
@@ -257,6 +260,15 @@ if (msg.sender !=address s(_vrfCoordinator)) {
         }
         //but newer versions allow us to add errors in the require statement
         //   require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
+    if(s_raffleState != RaffleState.OPEN){
+        revert Raffle__RaffleNotOpen();
+    }
+        //if they pay enough we add them to the players array
+       //we push user to array
+        s_players.push(payable(msg.sender)); //msg.sender is of type address so we need to convert it to payable address
+        //we emit an event when someone enters the raffle
+        emit RaffleEntered(msg.sender);
+    
     }
     event RaffleEntered(address indexed s_player);
     //getter functions
@@ -268,3 +280,16 @@ if (msg.sender !=address s(_vrfCoordinator)) {
 }
 //CHAINLINK VRF
 //VRF is done in 2 transactions
+
+
+///focus on tests and dev ops
+/**TESTS
+ * 1.Write deploy scripts
+ *  1.Note, these will not work on ZKsync
+ * 2.Write tests
+ * 1.Local Chain
+ * 2. Forked testnet
+ * 3.Forked mainnet
+ * 3.Write staging tests
+ * 
+ * */
