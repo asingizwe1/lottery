87d6a93aafedb144d9c269353d378f46693d6120
asingizwe1 <asingizwe1louis1@gmail.com>
2025-12-19 07:44:35 +0300

    Unit test file set up

---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,11 @@
+//SPDX License-Identifier: MIT
+pragma solidity ^0.8.18;    
+
+import {Test} from "forge-std/Test.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {Raffle} from "../../Raffle.sol";
+
+
+contract RaffleTest is Test {
+    
+}
